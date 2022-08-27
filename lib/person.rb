358bbdab7086a21d8cf5ec689e35eec8f312c83b
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-08-27 10:07:36 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
+
+
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, val|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", val)
+    end
+  end
 end
